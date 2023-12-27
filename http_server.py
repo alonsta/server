89ae4f64295b9e28cc8a7ee59f4ab2fc9c92552e
alonsta <130247709+alonsta@users.noreply.github.com>
2023-12-27 18:48:39 +0200

    Update http_server.py

--- http_server.py
+++ http_server.py
@@ -3,12 +3,11 @@ import socket
 
 def main():
     port = 80
-    if True:
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.bind(('0.0.0.0', port))
-        print('Server is up and running')
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', port))
+    print('Server is up and running')
+    while True:
         try:
-
             server_socket.listen()
             client_socket, client_address = server_socket.accept()
             print(client_address[0] + " connected")
@@ -19,45 +18,37 @@ def main():
 
 def serve_client(client_socket, client_address):
     typedict = {
+        "": "",
         "jpg": "Content-Type: image/jpeg\r\n",
         "txt": "Content-Type: text/html; charset=utf-8\r\n",
         "html": "Content-Type: text/html; charset=utf-8\r\n",
         "js": "Content-Type: text/javascript; charset=UTF-8\r\n",
         "css": "Content-Type: text/css\r\n"
     }
+    try:
+        data = client_socket.recv(2000).decode()
+        print("Client sent:" + data)
+        print("-------------")
 
-    while True:
-        try:
-            data = client_socket.recv(9999).decode()
-            print("Client sent:" + data)
-            print("-------------")
+        if "GET" == data[0:3] and "HTTP" in data.upper():
+            req_file = data.split("\r\n")[0].split(" ")[1]
+            version = "HTTP/1.0\r\n"
+            code = "200 OK\r\n"
+            end = req_file.split(".")[-1]
 
-            if "GET" == data[0:3] and "HTTP" in data.upper():
-                req_file = data.split("\r")[0][5:-9]
-                version = "HTTP 1.0\r"
-                code = "OK 200\r\n"
-                end = req_file.split(".")[-1]
-                if req_file == "":
-                    with open(r"webroot/index.html", "r") as file:
-                        site = file.read()
-                        ret = (version + code + typedict[end]).encode() + site.encode()
-                        print(ret)
-                        client_socket.send(ret)
-                elif end == "jpg":
-                    with open("webroot/" + req_file, "rb") as file:
-                        site = file.read()
-                        ret = (version + code + typedict[end]).encode() + site.hex().encode()
-                        print(ret)
-                        client_socket.send(ret)
-                else:
-                    with open("webroot/" + req_file, "r") as file:
-                        site = file.read()
-                        ret = (version + code + typedict[end]).encode() + site.encode()
-                        print(ret)
-                        client_socket.send(ret)
-        except Exception as e:
-            print(e)
-            print("connection terminated")
+            if req_file == "/":
+                req_file = "/index.html"
+
+            with open("webroot" + req_file, "rb") as file:
+                site = file.read()
+                content_type_header = typedict.get(end, "")
+                content_length_header = f"Content-Length: {len(site)}\r\n"
+                response = (version + code + content_type_header + content_length_header + "\r\n").encode() + site
+                client_socket.send(response)
+
+    except Exception as e:
+        print(e)
+        print("connection terminated")
 
 
 '''def assemble_response():
@@ -69,4 +60,3 @@ def serve_client(client_socket, client_address):
 
 if __name__ == "__main__":
     main()
-
